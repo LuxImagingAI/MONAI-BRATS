c87fdb26bf5def70f91d183839296bae67dabe70
raffi <raphaelmaser@posteo.de>
2022-10-04 13:01:45 +0200

    with validation

--- brats_train.py
+++ brats_train.py
@@ -4,10 +4,14 @@ import tempfile
 import time
 import matplotlib.pyplot as plt
 from monai.apps import DecathlonDataset
-from monai.engines import SupervisedTrainer
+from monai.engines import (
+    SupervisedTrainer,
+    SupervisedEvaluator
+)
 from monai.config import print_config
 from monai.data import DataLoader, decollate_batch
 from monai.handlers.utils import from_engine
+from monai.handlers import MetricLogger
 from monai.losses import DiceLoss
 from monai.inferers import sliding_window_inference
 from monai.metrics import DiceMetric
@@ -36,9 +40,12 @@ from monai.utils import set_determinism
 from brats_transforms import *
 import os
 import torch
+import pickle
 
 root_dir = "data"
+output_dir = "output"
 os.makedirs(root_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
 set_determinism(seed=0)
 
 # here we don't cache any data in case out of memory issue
@@ -82,6 +89,7 @@ loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_oneh
 optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
 
+# create handlers for training procedure
 dice_metric = DiceMetric(include_background=True, reduction="mean")
 dice_metric_batch = DiceMetric(include_background=True, reduction="mean_batch")
 
@@ -113,16 +121,69 @@ def inference(input):
 torch.backends.cudnn.benchmark = True
 
 # create training procedure
-trainer = SupervisedTrainer(device=device, max_epochs=1, train_data_loader=train_loader, network=model, optimizer=optimizer, loss_function=loss_function, key_train_metric={"dice_metric":dice_metric, "dice_metric_batch":dice_metric_batch},amp=False)
+trainer = SupervisedTrainer(device=device, max_epochs=1, train_data_loader=train_loader, network=model, optimizer=optimizer, loss_function=loss_function,amp=False)
 
 @trainer.on(Events.EPOCH_COMPLETED)
 def _print_loss(engine):
     print(f"Epoch {engine.state.epoch}/{engine.state.max_epochs} Loss: {engine.state.output[0]['loss']}")
     lr_scheduler.step()
 
+
+metric_values = []
+metric_values_tc = []
+metric_values_wt = []
+metric_values_et = []
+
+@trainer.on(Events.EPOCH_COMPLETED)
+def _compute_score(engine):
+    epoch = engine.state.epoch
+    model.eval()
+    with torch.no_grad():
+
+        for val_data in val_loader:
+            val_inputs, val_labels = (
+                val_data["image"].to(device),
+                val_data["label"].to(device),
+            )
+            val_outputs = inference(val_inputs)
+            val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
+            dice_metric(y_pred=val_outputs, y=val_labels)
+            dice_metric_batch(y_pred=val_outputs, y=val_labels)
+
+        metric = dice_metric.aggregate().item()
+        metric_values.append(metric)
+
+        # dice_metric_batch returns 3 different values (one for each output channel)
+        metric_batch = dice_metric_batch.aggregate()
+        metric_tc = metric_batch[0].item()
+        metric_values_tc.append(metric_tc)
+        metric_wt = metric_batch[1].item()
+        metric_values_wt.append(metric_wt)
+        metric_et = metric_batch[2].item()
+        metric_values_et.append(metric_et)
+
+        dice_metric.reset()
+        dice_metric_batch.reset()
+
+        print(
+            f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+            f" tc: {metric_tc:.4f} wt: {metric_wt:.4f} et: {metric_et:.4f}"
+        )
+
+# run the training procedure
 trainer.run()
 
-torch.jit.script(model).save("classifier.zip")
+metric_dict = {
+    "metric_values": metric_values,
+    "metric_values_tc": metric_values_tc,
+    "metric_values_wt": metric_values_wt,
+    "metric_values_et": metric_values_et
+}
+
+# save model and metrics
+with open(os.path.join("output","metric.pkl"), "wb") as handle:
+    pickle.dump(metric_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+torch.jit.script(model).save(os.path.join(output_dir,"trained_model.zip"))
 
 
 
