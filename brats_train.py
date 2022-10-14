dc3c22e2cecdb069629cdc8914011c45dd2b57a2
raffi <raphaelmaser@posteo.de>
2022-10-14 21:44:11 +0200

    error in cross-validation, included val_loader for lr_finder

--- brats_train.py
+++ brats_train.py
@@ -92,7 +92,7 @@ dice_metric_batch = DiceMetric(include_background=True, reduction="mean_batch")
 
 # Learning Rate Finder
 lr_finder = LearningRateFinder(model=deepcopy(model), optimizer=optimizer, criterion=deepcopy(loss_function), device=device)
-lr_finder.range_test(train_loader=deepcopy(train_loader), val_loader=val_loader, start_lr=1e-4, end_lr=1, num_iter=100)
+lr_finder.range_test(train_loader=deepcopy(train_loader), start_lr=1e-4, end_lr=1, num_iter=100)
 lr, _ = lr_finder.get_steepest_gradient()
 print(f"Optimal learning rate found: lr=", lr)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
