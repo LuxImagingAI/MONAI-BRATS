8201b567eab904becc076b0cec6207eb6cd77109
raffi <raphaelmaser@posteo.de>
2022-12-11 14:27:50 +0100

    installing conda in HOME, much faster now (libmamba-solver)

--- env/install_conda.sh
+++ env/install_conda.sh
@@ -4,7 +4,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
-conda install -n base conda-libmamba-solver
+conda install -y -n base conda-libmamba-solver
 conda config --set solver libmamba
 
 
