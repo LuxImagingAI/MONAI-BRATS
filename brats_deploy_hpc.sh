7b2807f335e629ce8c2f42d453dcb5549f4bc87e
raffi <raphaelmaser@posteo.de>
2022-12-16 17:24:37 +0100

    removed conda init

--- brats_deploy_hpc.sh
+++ brats_deploy_hpc.sh
@@ -10,6 +10,8 @@
 sleep 2s
 
 nvidia-smi
+export PATH="$HOME/miniconda/bin:$PATH"
 source activate MONAI-BRATS
+
 python brats_deploy.py --input "data/Task01_BrainTumour/imagesTr" --output "output/labels" --model "output/models"
 
