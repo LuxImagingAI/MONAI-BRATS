46552f42132254c46ccd149b1cadb15fdfbc8767
raffi <raphaelmaser@posteo.de>
2022-12-16 17:17:47 +0100

    removed conda init

--- brats_train_hpc.sh
+++ brats_train_hpc.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 7                # Cores assigned to each tasks
 #SBATCH -p gpu              # Job-type, can be batch (only CPU) or gpu
 #SBATCH -G 1                # Number of GPUs for the job
-#SBATCH --time=0-16:00:00   # Time limit
+#SBATCH --time=0-30:00:00   # Time limit
 #SBATCH --array=0-3         # Definition of job array
 #SBATCH --qos normal
 
@@ -13,6 +13,9 @@ epochs=100
 sleep 2s
 
 nvidia-smi
+
+export PATH="$HOME/miniconda/bin:$PATH"
 source activate MONAI-BRATS
+
 python brats_train.py --nfolds ${SLURM_ARRAY_TASK_COUNT} --fold ${SLURM_ARRAY_TASK_ID} --epochs $epochs
 
